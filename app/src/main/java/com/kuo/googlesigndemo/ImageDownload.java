716df6875701877aab8b1c2e14b5c6c2ffeaef6c
AndyKuo <k.kuanwei@gmail.com>
2016-06-13 15:22:56 +0800

    Create Project

---
+++ app/src/main/java/com/kuo/googlesigndemo/ImageDownload.java
@@ -0,0 +1,60 @@
+package com.kuo.googlesigndemo;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+import java.net.URI;
+
+/**
+ * Created by Kuo on 2016/6/13.
+ */
+public class ImageDownload extends AsyncTask<Uri, Integer, Bitmap> {
+
+    private ImageView imageView;
+
+    private ProgressDialog progressDialog;
+
+    public ImageDownload(Context context, ImageView imageView) {
+        this.imageView = imageView;
+
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setTitle("我會慢慢等、慢慢等、慢慢等～");
+        progressDialog.show();
+    }
+
+    @Override
+    protected Bitmap doInBackground(Uri... params) {
+
+        Uri uri = params[0];
+        Bitmap bitmap = null;
+
+        try {
+            URI uri1 = new URI(uri.toString());
+            InputStream in = uri1.toURL().openStream();
+            bitmap = BitmapFactory.decodeStream(in);
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+
+        return bitmap;
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        progressDialog.setProgress(values[0]);
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap bitmap) {
+        progressDialog.dismiss();
+        imageView.setImageBitmap(bitmap);
+    }
+}
---
+++ app/src/main/java/com/kuo/googlesigndemo/MainActivity.java
@@ -0,0 +1,99 @@
+package com.kuo.googlesigndemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener{
+    //AF:08:00:A8:A2:A0:28:42:0F:4F:48:4E:CF:83:FE:FC:66:19:91:E5
+
+    private static final int RC_SIGN_IN = 0;
+
+    private static final String TAG = "GoogleSignDemo";
+
+    private TextView nameText, emailText;
+
+    private ImageView photoImage;
+
+    private GoogleApiClient mGoogleApiClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+        signInButton.setScopes(gso.getScopeArray());
+        signInButton.setOnClickListener(this);
+
+        nameText = (TextView) findViewById(R.id.nameText);
+        emailText = (TextView) findViewById(R.id.emailText);
+
+        photoImage = (ImageView) findViewById(R.id.photoImage);
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.d(TAG, connectionResult.getErrorMessage());
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+        }
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+            nameText.setText(acct.getDisplayName());
+            emailText.setText(acct.getEmail());
+
+            new ImageDownload(this, photoImage).execute(acct.getPhotoUrl());
+        }
+    }
+}
